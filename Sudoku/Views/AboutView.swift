c787cb2d0cdf5c9d29cbbe5660830ef16549ef10
imoofs <imoofs@gmail.com>
2025-07-15 11:46:11 -0700

    feat: add aout and help views to making initial file struct

--- Sudoku/Views/AboutView.swift
+++ Sudoku/Views/AboutView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+/// Type: Navigation Stack
+///
+/// - 1개의 NavigationBarItem을 갖는다
+///     -- Tile: Home
+///     -- Action: GameHomeView 열기
 struct AboutView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
